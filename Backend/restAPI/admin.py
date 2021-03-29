9b57cb8b57da331e8ff7d518a899467c12de9131
porciuCato <mpcato@naver.com>
2021-03-29 16:21:59 +0900

    refactor: delete django app

--- Backend/restAPI/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Human
-
-class HumanAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'address', )
-
-admin.site.register(Human, HumanAdmin)
-
