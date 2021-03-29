9e7a29a16b83bc444cd8f9b1248c824a1f3fe2dc
porciuCato <mpcato@naver.com>
2021-03-30 00:10:56 +0900

    chore: make class type component

---
+++ Frontend/page/src/classApp.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// 클래스형 컴포넌트에는 render 함수가 꼭 있어야하며 반환하는 JSX가 있어야 한다.
+class classApp extends Component {
+  render() {
+    const name = '리액트';
+    return <div style="react">{name}</div>;
+  }
+}
