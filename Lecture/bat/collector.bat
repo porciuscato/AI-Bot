1a3c2111b1f066d0dfcd3fd536b46634bc5c6cbc
porciusCato <mpcato@naver.com>
2021-04-26 23:07:59 +0900

    feat: set mysql auto restart

--- Lecture/bat/collector.bat
+++ Lecture/bat/collector.bat
@@ -18,7 +18,7 @@ goto repeat
 
 :1
 set x=0
-set max=1500
+set max=2000
 
 start python "%~dp0/../collector_v3.py"
 timeout 5 > NUL
--- Lecture/bat/restart_mysql80.bat
+++ Lecture/bat/restart_mysql80.bat
@@ -1,2 +1,3 @@
-powershell.exe -command "& 'C:\Users\Jinhong\git\BotProject\Lecture\bat\restart_mysql80.ps1'"
+@REM powershell.exe -windowstyle hidden -command "& 'C:\Users\Jinhong\git\BotProject\Lecture\bat\restart_mysql80.ps1'"
+powershell.exe -windowstyle hidden -file C:\Users\Jinhong\git\BotProject\Lecture\bat\restart_mysql80.ps1
 
